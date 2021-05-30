cc631f1e7d3bb04599f42af96fdfab1fdd05daa3
the-lensky <55101239+the-lensky@users.noreply.github.com>
2021-05-30 16:42:24 +0300

    -test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import './App.css'
+import Main from './components/Main'
+import Bookmarks from './components/BookmarkList'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Sidebar from './components/Sidebar'
+import { ImageProvider } from './contex/state'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    return (
+        <Router>
+            <main className='wrapper grey darken-2'>
+                <Header/>
+                <ImageProvider>
+                    <Sidebar/>
+                    <Switch>
+                        <Route path='/' exact component={Main}/>
+                        <Route path='/bookmarks' component={Bookmarks}/>
+                    </Switch>
+                </ImageProvider>
+                <Footer/>
+            </main>
+        </Router>
+    )
 }
 
-export default App;
+export default App
